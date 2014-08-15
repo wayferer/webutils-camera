f2a4500d26299bb3bde03f33bd401fb7819f5605
Abdullah Norozi Iranzad <a1norozi@gmail.com>
2014-08-14 20:23:47 -0600

    first pass at capture image functionality

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-angular.module("app", []);
+function picChange(event){
+	var fileInput = event.target.files;
+	if(fileInput.length>0){
+		var windowURL = window.URL || window.webkitURL;
+		var picURL = windowURL.createObjectURL(fileInput[0]);
+		var photoCanvas = zQ("#capturedPhoto");
+		var ctx = photoCanvas.getContext("2d");
+		var photo = new Image();
+		photo.onload = function(){
+			ctx.drawImage(photo, 0, 0, 200, 200);
+		};
+		photo.src = picURL;
+		windowURL.revokeObjectURL(picURL);
+	}
+}
