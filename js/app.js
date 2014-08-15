10ab320544f83cbd80f11ee81493386e52868eaa
Abdullah Norozi Iranzad <a1norozi@gmail.com>
2014-08-15 09:43:46 -0600

    capture of image from video stream to canvas working

--- js/app.js
+++ js/app.js
@@ -1,19 +1,3 @@
-//http://www.syntaxxx.com/accessing-user-device-photos-with-the-html5-camera-api/
-/*function picChange(event){
-	var fileInput = event.target.files;
-	if(fileInput.length>0){
-		var windowURL = window.URL || window.webkitURL;
-		var picURL = windowURL.createObjectURL(fileInput[0]);
-		var photoCanvas = zQ("#capturedPhoto");
-		var ctx = photoCanvas.getContext("2d");
-		var photo = new Image();
-		photo.onload = function(){
-			ctx.drawImage(photo, 0, 0, 200, 200);
-		};
-		photo.src = picURL;
-		windowURL.revokeObjectURL(picURL);
-	}
-}*/
 //http://www.html5rocks.com/en/tutorials/getusermedia/intro/
 
 var video = document.getElementById('video');
@@ -31,7 +15,6 @@ if (navigator.getUserMedia) {
 }
 
 document.getElementById('btnSnapshot').onclick = snapshot;
-//document.getElementById('btnRedo').onclick = redo;
 
 var btnSnapshot = document.getElementById('btnSnapshot');
 var btnRedo = document.getElementById('btnRedo');
@@ -46,16 +29,9 @@ var y;
 	btnSnapshot.style.display = 'none';
 	btnRedo.style.display = 'block';
 	canvas.style.display = 'block';
-	ctx.drawImage(video, 0, 0);
-	// canvas.width = '200';
-	// canvas.height = '200';
-	ctx.fillStyle = "#000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(video, 0, 0);
+	canvas.width = video.videoWidth;
+	canvas.height = video.videoHeight;
+	ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 	video.style.display = 'none';
 	return false;//use preventdefault
 }
-
-/*window.onerror = function(a,b,c) {
-  alert(a + "; " + b + "; " + c);
-}*/
